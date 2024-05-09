4afae5dcff1073f7cba292c3b93f23ddc8a3c337
Eric_Lian <git@superexercisebook.com>
2024-05-10 00:44:12 +0800

    feat: get single resource

--- src/main/kotlin/para/party/plugins/Routing.kt
+++ src/main/kotlin/para/party/plugins/Routing.kt
@@ -1,12 +1,19 @@
 package para.party.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.netty.util.internal.logging.Slf4JLoggerFactory
+import org.ktorm.dsl.eq
 import org.ktorm.entity.add
+import org.ktorm.entity.filter
+import org.ktorm.entity.firstOrNull
+import org.ktorm.entity.toList
+import org.slf4j.LoggerFactory
 import para.party.db.db
 import para.party.entity.User
 import para.party.entity.users
@@ -16,6 +23,9 @@ import java.time.ZoneOffset
 import java.util.*
 
 fun Application.configureRouting() {
+    val logger = LoggerFactory.getLogger(this::class.java)
+
+
     install(AutoHeadResponse)
     routing {
         get("/") {
@@ -37,6 +47,17 @@ fun Application.configureRouting() {
             db.users.add(user)
 
             call.respond(user)
+        }
+
+        get("/users/{id}") {
+            val id = call.parameters["id"] ?: throw InvalidBodyException("?")
+            // val result = db.users.firstOrNull() { it.id eq id } ?: throw Exception("找不到")
+            // val result = db.users.filter { it.id eq id }.firstOrNull() ?: throw Exception("找不到")
+            // 不能使用上面那个，因为 sinodb 还支持 limit 0, 1;
+
+            val result = db.users.filter { it.id eq id }.toList().firstOrNull() ?: throw Exception("找不到")
+            // 得先查出来再取 first
+            call.respond(result)
         }
     }
 }
